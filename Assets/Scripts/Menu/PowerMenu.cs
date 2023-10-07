dae871921883766cc142875ecd6b39e78ef56841
Claire <45241465+claireecho@users.noreply.github.com>
2023-10-07 19:54:00 -0400

    Formatted Power Menu

--- Assets/Scripts/Menu/PowerMenu.cs
+++ Assets/Scripts/Menu/PowerMenu.cs
@@ -25,12 +25,10 @@ public class PowerMenu : MonoBehaviour
 
             Time.timeScale = 0;
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            gameObject.transform.GetChild(1).gameObject.SetActive(true);
 
         } else {
             Time.timeScale = 1;
             gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            gameObject.transform.GetChild(1).gameObject.SetActive(false);
         }
     }
 }
