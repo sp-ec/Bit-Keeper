0888c6b65a8d581c3b11626f799116c5d37cee40
Claire <45241465+claireecho@users.noreply.github.com>
2023-10-07 17:10:26 -0400

    Created Script for Power Menu

---
+++ Assets/Scripts/Menu/PowerMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerMenu : MonoBehaviour
+{
+
+    public bool enablePowerMenu = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enablePowerMenu = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space")) {
+            enablePowerMenu = !enablePowerMenu;
+        }
+
+        if (enablePowerMenu) {
+
+            Time.timeScale = 0;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+
+        } else {
+            Time.timeScale = 1;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
