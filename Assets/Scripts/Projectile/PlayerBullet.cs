340669d270565a56dfb971aa0d83f7456391d170
Spectrobyte <48649161+Spectromeda@users.noreply.github.com>
2023-10-07 01:09:46 -0400

    added bullet collision

--- Assets/Scripts/Projectile/PlayerBullet.cs
+++ Assets/Scripts/Projectile/PlayerBullet.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class PlayerBullet : Projectile
 {
-
+    protected override void RunCollision(RaycastHit2D hit){
+        if (hit.collider.CompareTag("Bug")){
+            Bug bugScript = hit.transform.gameObject.GetComponent<Bug>();
+            bugScript.damageEnemy();
+        }
+        base.RunCollision(hit);
+    }
 }
